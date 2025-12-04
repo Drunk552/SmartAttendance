f1e3a6c12853f9a7f16ff82538cc1fb7eaa41c17
Inside0105 <772401155@qq.com>
2025-12-04 18:20:41 +0800

    完善Epic3的所有功能

--- src/business/face_demo.h
+++ src/business/face_demo.h
@@ -1,12 +1,26 @@
-// src/business/face_demo.h
-#pragma once
-#include <opencv2/core.hpp>
+// src/business/face_detect.h
+#ifndef FACE_DEMO_H
+#define FACE_DEMO_H
 
-// 初始化函数
-bool business_init();
+#include "db_storage.h"//数据层头文件
 
-// 保存图像数据的函数声明（使用 C++ cv::Mat 类型）
-bool data_saveImage(const cv::Mat &image);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+bool business_init();// 初始化函数
+bool business_processAndSaveImage(const cv::Mat& inputImage);//请求业务层处理并保存函数声明
+bool test_business_processAndSaveImage(const std::string& imagePath);//测试business_processAndSaveImage接口声明
+bool test_convertToGrayscale(const std::string& imagePath);//测试convertToGrayscale函数声明
+
 
 // 单次运行函数
 void business_run_once();
+
+cv::Mat convertToGrayscale(const cv::Mat& inputImage);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // FACE_DEMO_H
