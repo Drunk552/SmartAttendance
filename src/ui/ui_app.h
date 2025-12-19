f590568381e744292432fe1b1564f9546a483baf
drunk552 <drunk180504@outlook.com>
2025-12-19 18:29:59 +0800

    修改ui层源码为c++类型

--- src/ui/ui_app.h
+++ src/ui/ui_app.h
@@ -3,17 +3,9 @@
 
 #include<stdbool.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 void ui_init(void);//初始化UI系统
 
 // 全局退出标志，供 main.cpp 检查
 extern volatile bool g_program_should_exit;
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
